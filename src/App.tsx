f180851fda4aaff0185adf1b04a1b14357d064f0
Josh Lee <joshlee.dev@gmail.com>
2024-09-24 12:21:59 +0100

    Implemented functionality on Hub component. Refreshing Agent details now updates credits and shipcount states. Created interface for Location and states for location. Location now displayed from state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate  } from "react-router-dom";
 import LogIn from "./pages/logIn/LogIn";
@@ -58,6 +58,8 @@ function App() {
                 token={token}
                 headquarters={headquarters}
                 accountID={accountID}
+                setCredits={setCredits}
+                setShipCount={setShipCount}
               />
           </> 
           : <Navigate to="/login" />} 
--- src/pages/hub/Hub.tsx
+++ src/pages/hub/Hub.tsx
@@ -1,23 +1,55 @@
+import { useState, useEffect } from "react"
 import * as APIWrapper from "../../api/spaceTradersAPI"
-
 
 interface HubProps {
   token: string;
   headquarters: string;
   accountID: string;
+  setCredits: (credits: number) => void;
+  setShipCount: (shipCount: number) => void;
+}
+
+interface Location {
+  systemSymbol: string;
+  symbol: string;
+  type: string;
+  x: number;
+  y: number;
+  orbitals: { symbol: string }[];
+  traits: {
+    symbol: string;
+    name: string;
+    description: string;
+  }[];
+  faction: {
+    symbol: string;
+  };
 }
 
 const Hub = ({
   token,
   headquarters,
   accountID,
+  setCredits,
+  setShipCount,
 }: HubProps) => {
+  const [startingLocation, setStartingLocation] = useState<Location | null>(null);
+
+  const refreshAgentDetails = (credits: number, shipCount: number) => {
+    setCredits(credits);
+    setShipCount(shipCount);
+    console.log(shipCount);
+  }
+
+  useEffect(() => {
+    handleViewStartingLocation();
+  }, []);
 
   const handleViewAgentDetails = async () => {
     try {
       const result = await APIWrapper.viewAgentDetails(token);
       if (result) {
-        console.log(result);
+        refreshAgentDetails(result.data.credits, result.data.shipCount)
       }
     } catch (error) {
       console.error('Error fetching agent data:', error);
@@ -29,6 +61,7 @@ const Hub = ({
       const result = await APIWrapper.viewStartingLocation(token);
       if (result) {
         console.log(result);
+        setStartingLocation(result.data);
       }
     } catch (error) {
       console.error('Error fetching location data:', error);
@@ -36,14 +69,32 @@ const Hub = ({
   };
 
   return (
-    <div>
-      
+    <div className="hub-container">
       <h1>Hub</h1>
-      <p>{headquarters}</p>
-      <p>{accountID}</p>
+      <div className="agent-details-wrapper">
+        <h2>Agent Details:</h2>
+        <p>Account ID: {accountID}</p>
+        <p>Headquarters: {headquarters}</p>
+        
+        <button onClick={handleViewAgentDetails}>Refresh</button>
+      </div>
       
-      <input type="submit" value="show agent" onClick={handleViewAgentDetails} />
-      <input type="submit" value="show starting location" onClick={handleViewStartingLocation} />
+      <div className="location-details-wrapper">
+        <h2>Location Details:</h2>
+        {startingLocation ? (
+          <div>
+            <p>System: {startingLocation.systemSymbol}</p>
+            <p>Faction: {startingLocation.faction.symbol}</p>
+            <p>Type: {startingLocation.type}</p>
+            <p>Number of Orbitals: {startingLocation.orbitals.length}</p>
+            <p>Number of Traits: {startingLocation.traits.length}</p>
+            <p>Coordinates: ({startingLocation.x}, {startingLocation.y})</p>
+          </div>
+        ) : (
+          <p>Loading location data...</p>
+        )}
+        <button onClick={handleViewStartingLocation}>Refresh</button>
+      </div>
     </div>
   );
 }
