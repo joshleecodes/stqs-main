4a22c0ac7f0f4ad3623bfb0a99db193b4ea25222
Josh Lee <joshlee.dev@gmail.com>
2024-09-24 09:08:11 +0100

    Contracts now load when page mounts rather than waiting for user interaction

--- src/pages/contracts/Contracts.tsx
+++ src/pages/contracts/Contracts.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import * as APIWrapper from "./../../api/spaceTradersAPI"
 
 interface ContractsProps {
@@ -33,65 +33,69 @@ token,
 }: ContractsProps) => {
   const [contracts, setContracts] = useState<Contract[]>([]);
 
-    const handleViewContracts = async () => {
-      try {
-        const result = await APIWrapper.viewContracts(token);
-        if (result) {
-          setContracts(result.data);
-          console.log(result);
-        }
-      } catch (error) {
-        console.error('Error fetching contracts:', error);
-      }
-    };
+  useEffect(() => {
+    handleViewContracts();
+  }, []);
 
-    const acceptContract = async (contractID: string) => {
-      try {
-        const result = await APIWrapper.acceptContract(token, contractID);
-        if (result) {
-          console.log(result);
-        }
-      } catch (error) {
-        console.error('Error accepting contract:', error);
+  const handleViewContracts = async () => {
+    try {
+      const result = await APIWrapper.viewContracts(token);
+      if (result) {
+        setContracts(result.data);
+        console.log(result);
       }
+    } catch (error) {
+      console.error('Error fetching contracts:', error);
     }
+  };
 
-    
+  const acceptContract = async (contractID: string) => {
+    try {
+      const result = await APIWrapper.acceptContract(token, contractID);
+      if (result) {
+        console.log(result);
+      }
+    } catch (error) {
+      console.error('Error accepting contract:', error);
+    }
+  }
 
-    return(<>
-        <h1>Contracts</h1>
-        <input type="submit" value="View Contracts" onClick={handleViewContracts} />
+  
 
-        {contracts.length > 0 ? (
-        <div className="contract-list-wrapper">
-          {contracts.map((contract) => (
-            <div key={contract.id} className="contract-wrapper">
-              <p>Contract Type: {contract.type}</p>
-              <p>Accepted: {contract.accepted? 'Yes' : 'No'}</p>
-              <p>Deadline to Accept: {new Date(contract.deadlineToAccept).toLocaleString()}</p>
-              <p>Completed: {contract.fulfilled? 'Yes' : 'No'}</p>
-              <h3>Terms:</h3>
-              <p>Deadline: {new Date(contract.terms.deadline).toLocaleString()}</p>
-              <p>Payment on Accept: {contract.terms.payment.onAccepted}</p>
-              <p>Payment on Fulfill: {contract.terms.payment.onFulfilled}</p>
-              <h3>Delivery Terms: </h3>
-              <ul>
-                {contract.terms.deliver.map((item, index) => (
-                  <li key={index}>
-                    {item.unitsRequired} {item.tradeSymbol} to {item.destinationSymbol}
-                  </li>
-                ))}
-              </ul>
-              {!contract.accepted && (
-                <input type="submit" value="Accept Contract" onClick={() => acceptContract(contract.id)} />
-              )}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>No contracts available.</p>
-      )}
-    </>)
+  return(<>
+      <h1>Contracts</h1>
+      <input type="submit" value="Refresh Contracts" onClick={handleViewContracts} />
+
+      {contracts.length > 0 ? (
+      <div className="contract-list-wrapper">
+        {contracts.map((contract) => (
+          <div key={contract.id} className="contract-wrapper">
+            <p>Contract Type: {contract.type}</p>
+            <p>Accepted: {contract.accepted? 'Yes' : 'No'}</p>
+            <p>Deadline to Accept: {new Date(contract.deadlineToAccept).toLocaleString()}</p>
+            <p>Completed: {contract.fulfilled? 'Yes' : 'No'}</p>
+            <h3>Terms:</h3>
+            <p>Deadline: {new Date(contract.terms.deadline).toLocaleString()}</p>
+            <p>Payment on Accept: {contract.terms.payment.onAccepted}</p>
+            <p>Payment on Fulfill: {contract.terms.payment.onFulfilled}</p>
+            <h3>Delivery Terms: </h3>
+            <ul>
+              {contract.terms.deliver.map((item, index) => (
+                <li key={index}>
+                  {item.unitsRequired} {item.tradeSymbol} to {item.destinationSymbol}
+                </li>
+              ))}
+            </ul>
+            {!contract.accepted && (
+              <input type="submit" value="Accept Contract" onClick={() => acceptContract(contract.id)} />
+            )}
+          </div>
+        ))}
+      </div>
+    ) : (
+      <p>No contracts available.</p>
+    )}
+  </>)
 
 }
 
